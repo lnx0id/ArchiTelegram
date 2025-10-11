4a532462dac28cf846914ebba72e0077f3f7eef0
Lnxoid <74073784+lnx0id@users.noreply.github.com>
2025-10-11 14:15:29 +0200

    Huh?!

--- BeholderArchi/DLC.cs
+++ BeholderArchi/DLC.cs
@@ -6,12 +6,12 @@ namespace BeholderArchi
 {
     internal class DLC
     {
-        private static int api_id = 2919669;
-        private static string api_hash = "0e858678d2ff61c95a6993799d895c25";
+        private static int api_id = 0;
+        private static string api_hash = "0";
 
-        private static Client app_client = new Client(api_id, api_hash); //Инициализация клиента
+        private static Client app_client = new Client(api_id, api_hash);
 
-        public static async Task<long> GetUserIdByUsernameAsync(string username, string token, Client client) //создаём метод, который будет принимать токен бота, клиента и юзернейм, а дальше в проекте просто вызываем этот метод
+        public static async Task<long> GetUserIdByUsernameAsync(string username, string token, Client client) //uh-huh
         {
             try
             {
